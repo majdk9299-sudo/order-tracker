1fe77289c0886027b12c0590ae20e87bfe5de4c3
majdk9299-sudo <majdk9299@gmail.com>
2026-07-30 01:56:20 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,39 @@
+const CACHE_NAME = "order-tracker-v1";
 const SHELL_FILES = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener("install", function (event) {
+self.addEventListener("install", function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
+    caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(SHELL_FILES);
     })
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", function (event) {
+self.addEventListener("activate", function(event) {
   event.waitUntil(
-    caches.keys().then(function (keys) {
+    caches.keys().then(function(keys) {
       return Promise.all(
-        keys.filter(function (key) { return key !== CACHE_NAME; })
-            .map(function (key) { return caches.delete(key); })
+        keys.filter(function(key) {
+          return key !== CACHE_NAME;
+        }).map(function(key) {
+          return caches.delete(key);
+        })
       );
     })
   );
-  self.clients.claim();
 });
 
-self.addEventListener("fetch", function (event) {
+self.addEventListener("fetch", function(event) {
   event.respondWith(
-    caches.match(event.request).then(function (cached) {
-      return cached || fetch(event.request);
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
